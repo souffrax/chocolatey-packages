8010b4dbcb3d8731a226a188acb22a1820f315ae
Alexander Georgievskiy <galeksandrp@gmail.com>
2016-10-05 22:57:40 +0300

    googlecode.com -> storage.googleapis.com

--- automatic/_output/syncless/2.1/tools/chocolateyInstall.ps1
+++ automatic/_output/syncless/2.1/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = 'syncless'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://big5sync.googlecode.com/files/Syncless 2.1.zip'
+$url = 'https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/big5sync/Syncless 2.1.zip'
 $checksum = '4e7a5cc25baa3a3aacedc5564ef2ee98cc7951d4'
 $checksumType = 'sha1'
 
