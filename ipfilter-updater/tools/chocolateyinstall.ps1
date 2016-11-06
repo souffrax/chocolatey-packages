29cae7f42b5cb23a206708bf83a8f9131d6a1976
Alexander Georgievskiy <galeksandrp@gmail.com>
2016-11-06 23:01:35 +0300

    2.2.2.0

---
+++ ipfilter-updater/tools/chocolateyinstall.ps1
@@ -0,0 +1,1 @@
+﻿Install-ChocolateyPackage "$env:chocolateyPackageName" 'msi' "MSIFASTINSTALL=1 DISABLEROLLBACK=1 /qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" "https://davidmoore.github.io/ipfilter/install/IPFilter.msi" -Checksum 'f048f18e2263e9ae909e7c8ab7a36e6bc19eeb0cc4aa9ec344606f7b86b5728a' -ChecksumType 'sha256'
