b7ad550b7af8387c65e3a4c2276e7cccbe75268b
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-09-22 12:01:23 +0300

    Init 2.5.3233

---
+++ advanced-ip-scanner/tools/chocolateyinstall.ps1
@@ -0,0 +1,1 @@
+﻿Install-ChocolateyPackage "$env:chocolateyPackageName" 'exe' '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' 'https://www.advanced-ip-scanner.com/download/ipscan25.exe' -Checksum '14e67c39fc99c315480f3e916c17fde6df5952bde763ef0c9f44cdc0bdabfec3' -ChecksumType 'sha256'
