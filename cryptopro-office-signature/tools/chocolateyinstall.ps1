b595d7ec130c674ca1cb9ca7f1921a78b9efe87e
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-09-26 17:19:57 +0300

    Init 2.0.11980

---
+++ cryptopro-office-signature/tools/chocolateyinstall.ps1
@@ -0,0 +1,1 @@
+﻿Install-ChocolateyPackage "$env:chocolateyPackageName" 'exe' '-silent -norestart' 'https://www.cryptopro.ru/sites/default/files/products/office-signature/2.0.11980/CryptoProOfficeSignature-2.0.11980.exe' -Checksum 'd50200e532873f045b6b403f03c8af2a18f7c51f155e6582b9609844c4265938' -ChecksumType 'sha256'
