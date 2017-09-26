558b44c4598cb8c2d118cd8ad1d5d5f16cfb78a3
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-09-26 18:24:37 +0300

    Init 3.8.0.180

---
+++ kontur-plugin/tools/chocolateyinstall.ps1
@@ -0,0 +1,1 @@
+﻿Install-ChocolateyPackage "$env:chocolateyPackageName" 'exe' '/S' 'https://help.kontur.ru/plugin/dist/3.8.0/kontur.plugin.exe' -Checksum '4cfdff31958c584be9f932fc4dd30f788da2d7af19582d42f566d144d1cb5eaf' -ChecksumType 'sha256'
