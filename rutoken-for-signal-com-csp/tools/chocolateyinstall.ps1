de52d3a98f5b7566f37b674853be9e93699f1896
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-09-27 02:47:10 +0300

    Init 2.55.00.0022

---
+++ rutoken-for-signal-com-csp/tools/chocolateyinstall.ps1
@@ -0,0 +1,1 @@
+﻿Install-ChocolateyPackage "$env:chocolateyPackageName" 'exe' '/s /v"/qb"' 'https://download.rutoken.ru/Rutoken/Support_SignalCom/2.55.00.0022/rt4SignalCOM.v.2.55.00.0022.exe' -Checksum '2528771d2e60bc07ecd704f3b69dca31ed6263a5a9d99d4cfa47cada1a3a4a30' -ChecksumType 'sha256'
