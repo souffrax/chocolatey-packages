d057582e8a68138f88519f5ebcef78adad6bfe3c
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-09-27 00:45:35 +0300

    [ci push] Init 4.5.25.8047

---
+++ kontur-certificates/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'exe'
+  SilentArgs             = '/nowait'
+  url                    = 'https://help.kontur.ru/content/components/Certificates_Kontur_Admin.exe'
+  checksum               = 'aee64989ab5a25e079210036b595fd364669aa9b4a83e7c4feaeafb6c30d324a'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyPackage @packageArgs
