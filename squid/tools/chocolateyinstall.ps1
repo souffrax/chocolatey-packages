ee8466073bc9b9a93a802faa9b30da893df15418
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-10-01 02:57:30 +0300

    Init 3.5.27

---
+++ squid/tools/chocolateyinstall.ps1
@@ -0,0 +1,1 @@
+﻿Install-ChocolateyPackage "$env:chocolateyPackageName" 'msi' '/qn /norestart' 'http://packages.diladele.com/squid/3.5.27/squid.msi' -Checksum 'a5bbc48e3b2b274a6625cd29b605b21b1b9241b319d3d1423fcc6baf9ebcaa66' -ChecksumType 'sha256'
