aca125cdb7ff614ff2f9fd421f1b85da17fe8889
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-10-02 16:46:55 +0300

    Init 3.5.1

---
+++ radmin-server/tools/chocolateyinstall.ps1
@@ -0,0 +1,1 @@
+﻿Install-ChocolateyPackage "$env:chocolateyPackageName" 'msi' '/qn /norestart' 'https://www.radmin.com/download/rserv351.msi' -Checksum '1a89747d65dc4b6199e37a08fe75d4ca39fd955f5a386051a592928d1107eff0' -ChecksumType 'sha256'
