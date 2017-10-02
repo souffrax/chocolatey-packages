2d1889afd2104d2008d17e03c77e3df32edc6e1e
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-10-02 18:28:23 +0300

    Init 2.1.0.2

---
+++ kontur-capicom/tools/chocolateyinstall.ps1
@@ -0,0 +1,6 @@
+﻿# Path to the folder where the script is executing
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+# Download from an HTTPS location
+Get-ChocolateyWebFile -PackageName "$env:chocolateyPackageName" -FileFullPath "$toolsDir\Capicom.zip" -Url 'https://www.kontur-extern.ru/Files/Modules/CmsFile/Capicom.zip' -Checksum '2728dcf1bf22e89d9ed46ad040be38dd21233cb4453d13d17d35ee9d03e5b092' -ChecksumType 'sha256'
+Get-ChocolateyUnzip -FileFullPath "$toolsDir\Capicom.zip" -Destination $toolsDir
+Install-ChocolateyPackage "$env:chocolateyPackageName" 'exe' '/S' "$toolsDir\capicom.exe" -Checksum '1119e17118f1d576489a8403323ddc41e86cd04980a6b399e89d213200169b2e' -ChecksumType 'sha256'
