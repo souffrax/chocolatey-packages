aa1d2bb712501e3c65860897f930bd72a724c56e
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-10-09 13:15:27 +0300

    [ci push] Init 2.3.STABLE5-NT-DELAYP

--- squid/tools/chocolateyinstall.ps1
+++ squid/tools/chocolateyinstall.ps1
@@ -1,1 +1,8 @@
-﻿Install-ChocolateyZipPackage "$env:chocolateyPackageName" 'http://squid.acmeconsulting.it/download/squid-2.3.STABLE5-NT-bin-EXTDNS.zip' "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)" -Checksum '8a06c47f4a7c365551a54e49927dc364ef0c388f623a2063e3eccd2e495f5db6' -ChecksumType 'sha256'
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  url                    = 'http://squid.acmeconsulting.it/download/squid-2.3.STABLE5-NT-bin-DELAYP.zip'
+  UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+  checksum               = 'd51d11ca8087b1e6e411e8faac028f7f59fb1591ac1f700aeaf52deb8bf969b1'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyZipPackage @packageArgs
