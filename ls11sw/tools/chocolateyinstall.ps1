dbb778f81854310001f0d5f1ec2b7af7677f1252
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-11-23 15:28:26 +0300

    [ci rdp] Init 1.0.0

---
+++ ls11sw/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'exe'
+  SilentArgs             = '/S'
+  url                    = 'http://soft.lissi.ru/docs/LS11SW/win/ls11sw-1.0.0-install-win32.exe'
+  url64                  = 'http://soft.lissi.ru/docs/LS11SW/win/LS11SW-1.0.0-install-win64.exe'
+  checksum               = 'e8bbaa7bd66d018d53dcaa717490f778cb726c98e27a7357b22b6d7567fe5180'
+  checksum64             = '46fe5009b27ba21cb461f82d872ea9b00ef32130587608463371b1ad37d68d53'
+  checksumType           = 'sha256'
+  checksumType64         = 'sha256'
+}
+Install-ChocolateyPackage @packageArgs
