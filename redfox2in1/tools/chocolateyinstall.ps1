41371a9568edc96c7339c4a8cc55b73aee01b480
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-11-23 15:28:26 +0300

    [ci push] Init 2.39

---
+++ redfox2in1/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  url                    = 'http://ftp.lissi.ru/SeaMonkey_2.39_GOST/SeaMonkey-2.39_GOST_WIN32.zip'
+  UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+  checksum               = '4bcf9877ba0ef1f7c64389a6a12595ce59dd4232fbeeda805a52f3c7c4230cc3'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyZipPackage @packageArgs
