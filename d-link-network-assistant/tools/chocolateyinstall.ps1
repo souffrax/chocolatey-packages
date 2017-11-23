412cf6e016f81aa98bb6913872091830da0b4477
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-11-23 15:28:26 +0300

    [ci push] Init 2.0.2.4

---
+++ d-link-network-assistant/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  url                    = 'ftp://ftp2.dlink.com/SOFTWARE/D-LINK_NETWORK_ASSISTANT/DNA_UTILITY_2.0.2.4_WIN_WW.ZIP'
+  checksum               = 'ceca6b8a29c7a0c5f61cb4523f7ad32ec45a07b495f059afe07c05397986ac61'
+  checksumType           = 'sha256'
+  UnzipLocation          = "$env:TMP"
+  SpecificFolder         = 'DNA_UTILITY_2.0.2.4_Setup.msi'
+}
+Install-ChocolateyZipPackage @packageArgs
+
+$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'msi'
+  SilentArgs             = '/qn /norestart'
+  File                   = "$env:TMP\DNA_UTILITY_2.0.2.4_Setup.msi"
+}
+Install-ChocolateyInstallPackage @packageArgs
