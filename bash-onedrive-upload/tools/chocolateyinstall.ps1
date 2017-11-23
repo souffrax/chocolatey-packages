e5257c26476159a5aeba6bd62eee93e4c3ffc7fe
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-11-23 15:28:26 +0300

    Init 0.1.0

---
+++ bash-onedrive-upload/tools/chocolateyinstall.ps1
@@ -0,0 +1,4 @@
+﻿git clone https://github.com/fkalis/bash-onedrive-upload.git "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\$env:chocolateyPackageName"
+cd "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\$env:chocolateyPackageName"
+git checkout 4419a7ffa9e227fcf225643a960a6f881edab363
+git submodule update --init --recursive
