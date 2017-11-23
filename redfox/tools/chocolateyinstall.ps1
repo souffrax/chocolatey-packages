479fe3b0c4e008c7d37b358e0a702cdceff02532
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-11-23 15:28:26 +0300

    [ci push] Init 52.0.1

---
+++ redfox/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  url                    = 'http://ftp.lissi.ru/FIREFOX_52/WIN32/Mozilla_Firefox_52.0.1esr_GOST_WIN32_.zip'
+  UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+  checksum               = '5ee5491aa32025f0c25f79e85fcd1b882d5fb27b46acbe3894f1810bc02e91b1'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyZipPackage @packageArgs
