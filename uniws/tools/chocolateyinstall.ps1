d0bb46516593937652d432f7120f857b7d35dfc6
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-11-23 15:28:26 +0300

    [ci push] Init 1.03

---
+++ uniws/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  url                    = 'http://www.wsgf.org/downloads/uniws.zip'
+  UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+  checksum               = 'c14f35e5bfe65cdda8de74e160183c0c6bf0602413bc32c26af0a3473e5a5c18'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyZipPackage @packageArgs
