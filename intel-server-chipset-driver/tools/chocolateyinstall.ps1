4aa87a43a4317d75b5d900dc55ae197bc2988103
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-11-23 15:28:26 +0300

    [ci rdp] Init 10.1.2.10

---
+++ intel-server-chipset-driver/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  url                    = 'https://downloadmirror.intel.com/25731/eng/Intel_chipset_10.1.2.10.zip'
+  checksum               = '39cbd82efc36ae272fa304a1ae52f75aaca63a1a60c5fb24785d723864735645'
+  checksumType           = 'sha256'
+  UnzipLocation          = "$env:TMP"
+}
+Install-ChocolateyZipPackage @packageArgs
+
+$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'exe'
+  SilentArgs             = '-s -norestart'
+  File                   = "$env:TMP\Intel_chipset_10.1.2.10\SetupChipset.exe"
+}
+Install-ChocolateyInstallPackage @packageArgs
