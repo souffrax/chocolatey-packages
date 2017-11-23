e7df2b4a6e027985298580357b2425bdebe0607b
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-11-23 15:28:26 +0300

    [ci push] Init 0.266

---
+++ signmaker/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'exe'
+  SilentArgs             = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  url                    = 'http://soft.lissi.ru/docs/SignMaker/SignMaker_setup.exe'
+  checksum               = 'b4d4e73d4d3f12a3a0ff985ecee04daa27d4e2906780f69f1d83f908921989dc'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyPackage @packageArgs
