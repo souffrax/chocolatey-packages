94ea1fa73b76ac65dfb6426d770a49f2c6cf3221
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-11-23 15:28:26 +0300

    [ci push] Init 52.0

---
+++ redfoxmail/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  url                    = 'http://ftp.lissi.ru/THUNDERBIRD_52/WIN32/Mozilla_Thunderbird_52.0_gost_WIN32.zip'
+  UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+  checksum               = '7cc3709d179092494440eb52b1cf2fdf04698b852e337273c123781fbd49dc75'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyZipPackage @packageArgs
