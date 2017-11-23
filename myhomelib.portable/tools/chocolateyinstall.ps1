8951116f700145b1a2275320dac51aec91304bee
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-11-23 15:28:26 +0300

    Init 2.2.0.822

---
+++ myhomelib.portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  url                    = 'http://home-lib.net/download/myhomelib/MyHomeLib_2_2.rar'
+  UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+  checksum               = '9fcba3545d8210ea0f646fe78f040eb52dadb779481d21f691f56e846f885b36'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyZipPackage @packageArgs
