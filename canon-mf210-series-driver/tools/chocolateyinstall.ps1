d61b340eb24d69362c4eccf4d7343b9568e25b49
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-11-23 15:28:26 +0300

    [ci skip] Init 21.20

---
+++ canon-mf210-series-driver/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'exe'
+  SilentArgs             = '/auto'
+  url                    = 'http://gdlp01.c-wss.com/gds/9/0100006239/02/MF210MFDriverV2120W32usEN.exe'
+  url                    = 'http://gdlp01.c-wss.com/gds/0/0100006240/02/MF210MFDriverV2120W64usEN.exe'
+  checksum               = 'be57154262df311e1293a79aa1f6d26eaac2d1d0d8e10990cc4344560da058db'
+  checksum64             = '3ebdad9abe953fcf1b5804b9632becfbd3c7fc357fe00104c967837d2f5a9d1a'
+  checksumType           = 'sha256'
+  checksumType64         = 'sha256'
+}
+Install-ChocolateyPackage @packageArgs
