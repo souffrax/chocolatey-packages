52638dfe030f24cc03ba8d8fcab75f916d0dda20
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-11-23 15:28:26 +0300

    [ci rdp] Init 0.1.0

---
+++ pkcs11confplugin/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'exe'
+  SilentArgs             = '/S'
+  url                    = 'http://soft.lissi.ru/docs/PKCS11Manager_Web/win/LSPK11ConfPlugin_setup.exe'
+  checksum               = '99da542d01ffc69e0a8eb223b7a60a4eacc66d31de452a3745d9acc5beb80d85'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyPackage @packageArgs
