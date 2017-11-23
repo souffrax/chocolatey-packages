3f5da88d22500b0d8ed13db3bd86af1d0b187ff8
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-11-23 15:28:26 +0300

    [ci push] Init 2004.04

---
+++ kkrieger/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  url                    = 'https://files.scene.org/get/parties/2004/breakpoint04/96kgame/kkrieger-beta.zip'
+  UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+  checksum               = '0de0b9abafd78cf7f57fb264c7136ace52b2653f5c675574d0f10eda0a654c9f'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyZipPackage @packageArgs
