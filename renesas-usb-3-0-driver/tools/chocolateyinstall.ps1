1ccadc1f2b4883749a4ecdc5eb54d5cc81591210
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-11-23 15:28:26 +0300

    [ci push] Init 2.1.28.1

---
+++ renesas-usb-3-0-driver/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'exe'
+  SilentArgs             = '/s /sp"/s"'
+  url                    = 'https://downloadmirror.intel.com/19880/eng/USB3.0_allOS_2.1.28.1_PV.exe'
+  checksum               = '8d13f085128f27c446664b1847efebd02f5383743fd053a9ae6dd05a4c49327d'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyPackage @packageArgs
