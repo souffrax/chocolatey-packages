70dd594c979ec3d374239f9a9bf1e78ba4fcf102
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-11-23 15:28:26 +0300

    Init 0.8.3

---
+++ tuxboot/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileFullPath           = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\tuxboot.exe"
+  url                    = 'https://downloads.sourceforge.net/project/tuxboot/0.8/Windows/tuxboot-0.8.3.exe'
+  checksum               = '7c7149929f5dd28ab32ba96a7c966a3727a9acce9ac70731187cfd05d0c3c7fa'
+  checksumType           = 'sha256'
+}
+Get-ChocolateyWebFile @packageArgs
