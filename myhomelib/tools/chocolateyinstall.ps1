3a5a86fd5bc32675a1177b9aafb689f77a8bdbee
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-11-23 15:28:26 +0300

    [ci push] Init 2.2.0.822

---
+++ myhomelib/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'exe'
+  SilentArgs             = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  url                    = 'http://home-lib.net/download/myhomelib/Setup_MyHomeLib_2_2.exe'
+  checksum               = 'a2debd3df212bdf883c9124136d404a99957f509c30337cbf1bacf3138878457'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyPackage @packageArgs
