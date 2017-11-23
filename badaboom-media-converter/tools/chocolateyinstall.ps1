d8ad4a12e56d86dc93e30f1a66028181fee97f02
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-11-23 15:28:26 +0300

    [ci push] Init 2.0.0.128

---
+++ badaboom-media-converter/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'exe'
+  SilentArgs             = '/S'
+  url                    = 'https://international.download.nvidia.com/geforce-com/international/applications/Badaboom_Setup_Trial_2.0.exe'
+  checksum               = '09584ed9899973f730d2671444b613a948c7c5b43752b98d6b22dac06fac1c46'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyPackage @packageArgs
