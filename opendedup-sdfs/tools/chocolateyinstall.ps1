6054d76e8af1444af7c250d9272da12464a75d49
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-11-23 15:28:26 +0300

    [ci rdp] Init 3.5.8.5

---
+++ opendedup-sdfs/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'exe'
+  SilentArgs             = '/S'
+  url                    = 'http://opendedup.org/odd/wp-content/uploads/2017/12/SDFS-3.5.8.5-Setup.exe'
+  checksum               = '6671459d4aab9c1113662c1ab84ba7856c040e9fb2acdac63dee5678ac8c069e'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyPackage @packageArgs
