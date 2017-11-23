5a5667bed755ee9715939a0c72b7dec27f9a709c
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-11-23 15:28:26 +0300

    Init 0.1.0

---
+++ lcsignplugin/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'exe'
+  SilentArgs             = '/S'
+  url                    = 'http://soft.lissi.ru/docs/LCSignPluginNSS/win/LCSignPlugin_setup.exe'
+  checksum               = '2395a79eb4b156bb5047f0f3b5677a53a478f0b2b0bd85377949cfe3da6c62d0'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyPackage @packageArgs
