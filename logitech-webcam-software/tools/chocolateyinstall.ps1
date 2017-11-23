31ef07f2443a26343185078432246a69f924cd55
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-11-23 15:28:26 +0300

    2.80.853.0

---
+++ logitech-webcam-software/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'exe'
+  SilentArgs             = '/S'
+  url                    = 'https://download01.logi.com/web/ftp/pub/video/lws/lws280.exe'
+  checksum               = 'a9cbba5638bea99f1d760dd979819026b5969bf7a4dd3591e3fa9639fab22142'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyPackage @packageArgs
