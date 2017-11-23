22752c6b1af2ac2d1d4c41a72103dbfc42758d05
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-11-23 15:28:26 +0300

    [ci push] Init 0.21

---
+++ zfs-win/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  url                    = 'https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/zfs-win/zfs-win-r21.zip'
+  UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+  checksum               = 'c4a4ed404ed66a4453193df9e9638d454fcf5b3d26126e7a6d3636fdb933e30e'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyZipPackage @packageArgs
