4c00054f28f0b59bff1f1b87bbded38b0cae0529
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-11-23 15:28:26 +0300

    [ci rdp] Init 1.0.0.0

---
+++ lslicplugin/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'exe'
+  SilentArgs             = '/S'
+  url                    = 'http://soft.lissi.ru/docs/plugin_license/LSLicPlugin_setup.exe'
+  checksum               = '5965b0bc5452b76566faebe49785d46bf72c43735b82a0830669c7bd713c6702'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyPackage @packageArgs
