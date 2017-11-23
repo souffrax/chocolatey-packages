a354882f82e4a9adc7de5c21b231e6fdee94adcd
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-11-23 15:28:26 +0300

    Init 1.0.1

---
+++ grubinst/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  url                    = 'https://downloads.sourceforge.net/project/grub4dos/grubinst/grubinst%201.0.1/grubinst_1.0.1_bin_win.zip'
+  UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+  checksum               = 'dd1cf6b26aeaf1214036728b9a892b96d53e116d4ff9c194b2ad18850d080def'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyZipPackage @packageArgs
