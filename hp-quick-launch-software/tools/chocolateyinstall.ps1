af2ad432371b08d98ebc75d5575319397d30f6ee
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-11-28 18:51:39 +0300

    [ci push] Init 2.60-WIN7

--- hp-quick-launch-software/tools/chocolateyinstall.ps1
+++ hp-quick-launch-software/tools/chocolateyinstall.ps1
@@ -1,1 +1,9 @@
-﻿Install-ChocolateyPackage "$env:chocolateyPackageName" 'exe' '' 'https://ftp.hp.com/pub/softpaq/sp56001-56500/sp56221.exe' -Checksum '3b1d15c3ea279b946618a3372c885f6673fbc99ea8c81928035472d03544ea3a' -ChecksumType 'sha256'
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'exe'
+  SilentArgs             = '/s'
+  url                    = 'https://ftp.hp.com/pub/softpaq/sp56001-56500/sp56221.exe'
+  checksum               = '3b1d15c3ea279b946618a3372c885f6673fbc99ea8c81928035472d03544ea3a'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyPackage @packageArgs
