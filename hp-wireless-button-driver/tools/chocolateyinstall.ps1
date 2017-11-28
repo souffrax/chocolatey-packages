cff397ebb62420fec3124bd3e67417aa076a000a
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-11-28 18:47:20 +0300

    Init 1.0.6.1-WIN8

---
+++ hp-wireless-button-driver/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'exe'
+  SilentArgs             = '/s /a /s /v"/qn /norestart"'
+  url                    = 'https://ftp.hp.com/pub/softpaq/sp59501-60000/sp59835.exe'
+  checksum               = 'd329fc0cd4160ad658fb6d6c80a7d9febb0986ad998373e66c7dc132e163942b'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyPackage @packageArgs
