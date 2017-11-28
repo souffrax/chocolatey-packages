545ab04492abccd181a6f7161d9ced8ecb3517ff
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-11-28 18:51:00 +0300

    [ci skip] Init F.12-RevA-AMD-Processors

--- hp-notebook-system-bios-update/tools/chocolateyinstall.ps1
+++ hp-notebook-system-bios-update/tools/chocolateyinstall.ps1
@@ -1,1 +1,1 @@
-﻿Install-ChocolateyPackage "$env:chocolateyPackageName" 'exe' '' 'https://ftp.hp.com/pub/softpaq/sp56001-56500/sp56186.exe' -Checksum '9fe70625aacb3c6a6f90a92670b62a261a0a320c1062352fc73dd43e050e7457' -ChecksumType 'sha256'
+﻿Install-ChocolateyPackage "$env:chocolateyPackageName" 'exe' '' 'https://ftp.hp.com/pub/softpaq/sp55001-55500/sp55377.exe' -Checksum '9fe70625aacb3c6a6f90a92670b62a261a0a320c1062352fc73dd43e050e7457' -ChecksumType 'sha256'
