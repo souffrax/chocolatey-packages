28c420d41ecce4e309406c366e261b7415d77c37
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-11-28 18:47:38 +0300

    [ci push] Init 7.41.216.2011-HP

---
+++ realtek-lan-driver/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'exe'
+  SilentArgs             = '/s /a /s'
+  url                    = 'https://ftp.hp.com/pub/softpaq/sp54501-55000/sp54998.exe'
+  checksum               = '131639e80c26d3a5a5887016a4b160212f7f5db43e95cb98df4e556cc031bf0d'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyPackage @packageArgs
