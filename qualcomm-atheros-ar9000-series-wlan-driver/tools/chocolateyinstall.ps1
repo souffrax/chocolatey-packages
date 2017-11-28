5bfa8edd3cd998895d7715d0e8685b564ba72d80
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-11-28 18:47:20 +0300

    [ci push] Init 1.00-WIN8-HP

---
+++ qualcomm-atheros-ar9000-series-wlan-driver/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'exe'
+  SilentArgs             = '/s'
+  url                    = 'http://ftp.hp.com/pub/softpaq/sp58001-58500/sp58498.exe'
+  checksum               = 'e998a508bb9f04735810488cbab49693862ce340f136cfa936a3a9196f96a1cf'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyPackage @packageArgs
