3fbbd680e2bdb891db5e3bebcc0c29b2de93421b
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-11-28 18:50:38 +0300

    Init 3.2.7.0-HP

--- ralink-wifi-adapter/tools/chocolateyinstall.ps1
+++ ralink-wifi-adapter/tools/chocolateyinstall.ps1
@@ -1,1 +1,9 @@
-﻿Install-ChocolateyPackage "$env:chocolateyPackageName" 'exe' '' 'https://ftp.hp.com/pub/softpaq/sp55501-56000/sp55715.exe' -Checksum '217002e5659b8913f521eb0e0666bbcc3b27bc0a1be429b1aca7d090f49be051' -ChecksumType 'sha256'
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'exe'
+  SilentArgs             = '/s'
+  url                    = 'https://ftp.hp.com/pub/softpaq/sp56001-56500/sp56036.exe'
+  checksum               = '217002e5659b8913f521eb0e0666bbcc3b27bc0a1be429b1aca7d090f49be051'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyPackage @packageArgs
