6533c815a85a057c1202a0262be60d7f24191c72
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-11-28 18:47:20 +0300

    Init 4.4.232.0-WIN8-HP

---
+++ validity-fingerprint-sensor-driver/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'exe'
+  SilentArgs             = '/s'
+  url                    = 'https://ftp.hp.com/pub/softpaq/sp58501-59000/sp58834.exe'
+  checksum               = 'b135c629702fd01ba3414a43545f0c47258c9e305730d1c0b718ff24521221bf'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyPackage @packageArgs
