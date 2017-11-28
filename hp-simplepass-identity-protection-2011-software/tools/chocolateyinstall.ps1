f93aa198414c4d699bef6227ad06464b62d4204b
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-11-28 18:51:25 +0300

    Init 5.4.0.402-WIN7

--- hp-simplepass-identity-protection-2011-software/tools/chocolateyinstall.ps1
+++ hp-simplepass-identity-protection-2011-software/tools/chocolateyinstall.ps1
@@ -1,1 +1,9 @@
-﻿Install-ChocolateyPackage "$env:chocolateyPackageName" 'exe' '' 'https://ftp.hp.com/pub/softpaq/sp56001-56500/sp56487.exe' -Checksum '' -ChecksumType 'sha256'
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'exe'
+  SilentArgs             = '/s /a /s /v"/qn /norestart"'
+  url                    = 'https://ftp.hp.com/pub/softpaq/sp56001-56500/sp56487.exe'
+  checksum               = '938771e17e8b8ffe1bea8ce6d64550a3fe94dae1d1d61e396402b2913aec5e37'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyPackage @packageArgs
