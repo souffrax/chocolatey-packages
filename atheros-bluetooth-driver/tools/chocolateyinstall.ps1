3b56a2c8debf8b0b824b945a9e03283ce6fba4ed
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-11-28 18:50:16 +0300

    [ci push] Init 1.00-WIN8-HP

--- atheros-bluetooth-driver/tools/chocolateyinstall.ps1
+++ atheros-bluetooth-driver/tools/chocolateyinstall.ps1
@@ -1,1 +1,9 @@
-﻿Install-ChocolateyPackage "$env:chocolateyPackageName" 'exe' '' 'https://ftp.hp.com/pub/softpaq/sp55001-55500/sp55343.exe' -Checksum '0753d0dd9621bfc31e986e9b6866110b6fd52f3a48968c99c0743a1c517677a7' -ChecksumType 'sha256'
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'exe'
+  SilentArgs             = '/s'
+  url                    = 'https://ftp.hp.com/pub/softpaq/sp58001-58500/sp58499.exe'
+  checksum               = 'e7d4b4d7a6da38703fc1fc508a97bd6b6b2f0a5711d7a0f39a488e2a43c313c1'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyPackage @packageArgs
