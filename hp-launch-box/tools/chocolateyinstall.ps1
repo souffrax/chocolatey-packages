5e6469832e3c07eec57c273bb246dfb55745eb81
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-11-28 18:48:29 +0300

    Init 1.10-WIN7

---
+++ hp-launch-box/tools/chocolateyinstall.ps1
@@ -0,0 +1,1 @@
+﻿Install-ChocolateyPackage "$env:chocolateyPackageName" 'exe' '/s' 'https://ftp.hp.com/pub/softpaq/sp56001-56500/sp56217.exe' -Checksum 'd957ab9ad275319d498994c855d68ed5da75cb7452cee40ac9b297142f1a0c09' -ChecksumType 'sha256'
