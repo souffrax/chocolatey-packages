b4d6c7c9c4b9db67d271d115abe3e8361ca5a425
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-11-28 18:47:16 +0300

    Init 16.2.10.12-WIN8-HP

---
+++ synaptics-touchpad-driver/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'exe'
+  SilentArgs             = '/s /a /S'
+  url                    = 'https://ftp.hp.com/pub/softpaq/sp58501-59000/sp58832.exe'
+  checksum               = '919fbee3019d870f5f13063921fb9e197433a8246d037d5409c25c39fa6bb035'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyPackage @packageArgs
