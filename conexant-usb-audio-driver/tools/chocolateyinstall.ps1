1f975cb816ee8f78876441eb5200580b1ff81477
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-11-28 18:47:00 +0300

    Init 1.29.10.0-HP

---
+++ conexant-usb-audio-driver/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'exe'
+  SilentArgs             = '/s'
+  url                    = 'https://ftp.hp.com/pub/softpaq/sp55001-55500/sp55461.exe'
+  checksum               = '9c9281dfb930648658000840fa84a27a1361d10e95d740b911a3b2f38ebb0da8'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyPackage @packageArgs
