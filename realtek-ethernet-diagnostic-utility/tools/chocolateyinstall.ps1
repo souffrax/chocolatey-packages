8c6fbe0106b27070877d2e5d4c37175f98659987
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-11-28 18:47:43 +0300

    [ci push] Init 2.0.3.0

---
+++ realtek-ethernet-diagnostic-utility/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  url                    = 'http://12244.wpc.azureedge.net/8012244/drivers/rtdrivers/cn/nic/0004-Diagnostic_v2.0.3.0_AllInOneRel_0803.zip'
+  checksum               = '8e3bbbb2267e1d300ccca6abb91ebfbe902e29617c57179527fd76d10c369b9e'
+  checksumType           = 'sha256'
+  UnzipLocation          = "$env:TMP"
+}
+Install-ChocolateyZipPackage @packageArgs
+
+$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'exe'
+  SilentArgs             = '/s'
+  File                   = "$env:TMP\Diagnostic_v2.0.3.0_AllInOneRel_0803\setup.exe"
+}
+Install-ChocolateyInstallPackage @packageArgs
