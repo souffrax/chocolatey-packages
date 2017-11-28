073a38906c1017d40d085a01b82988f085997228
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-11-28 18:51:53 +0300

    [ci skip] Init 5.1.0.0

--- hp-uefi-support-environment/tools/chocolateyinstall.ps1
+++ hp-uefi-support-environment/tools/chocolateyinstall.ps1
@@ -1,1 +1,1 @@
-﻿Install-ChocolateyPackage "$env:chocolateyPackageName" 'exe' '' 'https://ftp.hp.com/pub/softpaq/sp60501-61000/sp60844.exe' -Checksum '' -ChecksumType 'sha256'
+﻿Install-ChocolateyPackage "$env:chocolateyPackageName" 'exe' '' 'https://ftp.hp.com/pub/softpaq/sp60001-60500/sp60202.exe' -Checksum '' -ChecksumType 'sha256'
