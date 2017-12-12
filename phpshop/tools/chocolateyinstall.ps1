4cd13c38b057fc3f38ea79ad607ed1020731d68b
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-12-12 12:35:46 +0300

    Init 2.4

---
+++ phpshop/tools/chocolateyinstall.ps1
@@ -0,0 +1,2 @@
+﻿#Server geneate installer executable on-the-fly, so no checksums
+Install-ChocolateyPackage "$env:chocolateyPackageName" 'exe' '' 'http://www.phpshop.ru/loads/files/setup.exe' -Checksum '5234c8705b2bc3bb42119841397b96cf5b8e9e702edfcd9838db6f6e125f2320' -ChecksumType 'sha256'
