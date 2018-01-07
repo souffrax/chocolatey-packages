30d1b536a4627a93414e1560bca82545379f59ff
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-01-07 15:06:32 +0300

    Init 6.14.2.1577

---
+++ download-master/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'exe'
+  SilentArgs             = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  url                    = 'http://downloadmaster.ru/dm/archive/dmaster_6.14.2.1577_20.10.2017.exe'
+  checksum               = '05e848e4bbb87e42587ab4dfbd682c8dbcbaedf41714880f6c920d9cac7e1208'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyPackage @packageArgs
---
+++ download-master/tools/chocolateyuninstall.ps1
@@ -0,0 +1,4 @@
+﻿$key = Get-UninstallRegistryKey -SoftwareName "Download Master version $env:ChocolateyPackageVersion"
+
+#Uninstall-ChocolateyPackage "$env:chocolateyPackageName" 'exe' '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' "$key.UninstallString"
+Uninstall-ChocolateyPackage -PackageName "$env:chocolateyPackageName" -FileType 'exe' -SilentArgs '/SILENT /SUPPRESSMSGBOXES /NORESTART /SP-' -File $key.UninstallString.replace('"', '')
