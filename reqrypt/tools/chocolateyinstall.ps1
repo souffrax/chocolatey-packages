3580f513e5be6a57c0236c41c683c40fde63e7ae
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-01-07 04:30:39 +0300

    Init 1.3.1

---
+++ reqrypt/tools/chocolateyinstall.ps1
@@ -0,0 +1,1 @@
+﻿Install-ChocolateyPackage "$env:chocolateyPackageName" 'exe' '/S' 'https://reqrypt.org/download/reqrypt-1.3.1-win64-install.exe' -Checksum '0f109c1c0ecb42dcaa4251a833c709de0979b6c4cb88b3712dc1c998b6c0c284' -ChecksumType 'sha256'
