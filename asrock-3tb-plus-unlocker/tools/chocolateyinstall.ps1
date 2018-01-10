eef82f17faa0673dea52cedf28df52bd3a624dd9
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-01-10 13:58:51 +0300

    [ci push] Init 1.1.1

---
+++ asrock-3tb-plus-unlocker/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileFullPath           = "$env:TMP\3TBUnlocker(v1.1.1).zip"
+  url                    = 'http://asrock.pc.cdn.bitgravity.com/Utility/Others/3TBUnlocker(v1.1.1).zip'
+  checksum               = 'af089d31740b42969f1fec2e59bfc32bd8892d2ab5e42f7fd083431529077ae8'
+  checksumType           = 'sha256'
+}
+Get-ChocolateyWebFile @packageArgs
+
+$packageArgs = @{
+  FileFullPath           = "$env:TMP\3TBUnlocker(v1.1.1).zip"
+  Destination            = "$env:TMP"
+}
+Get-ChocolateyUnzip @packageArgs
+
+$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'exe'
+  SilentArgs             = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  File                   = "$env:TMP\3TBUnlocker(v1.1.1)\ASR3TB+Unlocker(v1.1.1).exe"
+}
+Install-ChocolateyInstallPackage @packageArgs
