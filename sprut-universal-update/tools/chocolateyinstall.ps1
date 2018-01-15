161dd934b04a9becbd8401caf05a30b12137540a
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-01-15 19:22:08 +0300

    [ci push] Init 2.72.60

---
+++ sprut-universal-update/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  url                    = 'http://www.gsm-sprut.com/files/file/SprutUniversal/v.2.72.60.rar'
+  UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+  checksum               = '53ab0d4188512a2364f3099f98da8aa072df46bedd1a61d2b9b4eb9bb1b76f74'
+  checksumType           = 'sha256'
+  SpecificFolder         = 'V2\Прошивка'
+}
+Install-ChocolateyZipPackage @packageArgs
