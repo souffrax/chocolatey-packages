b231f5a41a985c7d15273d296cb725017a6cf63d
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-01-15 16:09:31 +0300

    Init 2.08.4

---
+++ sprut-uinversal-config/tools/chocolateyinstall.ps1
@@ -0,0 +1,2 @@
+﻿Install-ChocolateyZipPackage "$env:chocolateyPackageName" 'http://www.gsm-sprut.com/files/file/SprutUniversal/v.2.72.60.rar' "$env:TMP" -Checksum '53ab0d4188512a2364f3099f98da8aa072df46bedd1a61d2b9b4eb9bb1b76f74' -ChecksumType 'sha256'
+Install-ChocolateyPackage "$env:chocolateyPackageName" 'exe' '/s /v"/qb"' "$env:TMP\V2\Конфигуратор\setup.exe" -Checksum '0d833a6b78a1ca02b12b1e74c5ac52336d4b5606ea16cbbfdbd1fce3cda98ef2' -ChecksumType 'sha256'
