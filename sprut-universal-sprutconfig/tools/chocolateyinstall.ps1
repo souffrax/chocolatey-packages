a7c1620440769e1822a31661a6bd8f2260b94649
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-01-15 19:22:08 +0300

    [ci push] Init 2.04.4000

---
+++ sprut-universal-sprutconfig/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  url                    = 'http://www.gsm-sprut.com/files/file/SprutUniversal/v.2.72.60.rar'
+  UnzipLocation          = "$env:TMP"
+  checksum               = '53ab0d4188512a2364f3099f98da8aa072df46bedd1a61d2b9b4eb9bb1b76f74'
+  checksumType           = 'sha256'
+  SpecificFolder         = 'V2\Конфигуратор'
+}
+Install-ChocolateyZipPackage @packageArgs
+
+mv "$env:TMP\V2\Конфигуратор\*" "$env:TMP"
+
+$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'exe'
+  SilentArgs             = '/s /v/qn'
+  File                   = "$env:TMP\setup.exe"
+}
+Install-ChocolateyInstallPackage @packageArgs
