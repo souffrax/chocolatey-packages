f498c73cf2782cd5639d75d46133544eac088f21
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-01-15 19:22:08 +0300

    Init 3.0

---
+++ sprut-sms-client/tools/chocolateyinstall.ps1
@@ -0,0 +1,2 @@
+﻿Install-ChocolateyZipPackage "$env:chocolateyPackageName" 'http://www.gsm-sprut.com/files/file/SprutUniversal/sms/smsserver.zip' "$env:TMP" -Checksum '70d59ef605cf28d03443c2d17529d045c7c69076007aeb233a24202b482a1cf0' -ChecksumType 'sha256'
+Install-ChocolateyInstallPackage "$env:chocolateyPackageName" 'exe' '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' "$env:TMP\sms_client_setup.exe"
