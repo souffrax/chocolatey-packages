42e073ba85d81408215192b427bb157f81db2a3a
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-01-26 12:56:21 +0300

    Update chocolateyinstall.ps1

--- litemanager-server/tools/chocolateyinstall.ps1
+++ litemanager-server/tools/chocolateyinstall.ps1
@@ -1,2 +1,2 @@
 ﻿Install-ChocolateyZipPackage "$env:chocolateyPackageName" 'http://litemanager.com/soft/litemanager_4.8.zip' "$env:TMP" -Checksum '5e27c47fac38ef31467776b3872a2033c599d3cbd91cfbd5109e84509f0e14b2' -ChecksumType 'sha256'
-Install-ChocolateyPackage "$env:chocolateyPackageName" 'msi' '/qn /norestart' "$env:TMP\LiteManager Pro - Server.msi" -Checksum 'e9c235c3da50373c58bd253524f695bdb8d797760e20197b12f875b9bc2217c8' -ChecksumType 'sha256'
+Install-ChocolateyInstallPackage "$env:chocolateyPackageName" 'msi' '/qn /norestart' "$env:TMP\LiteManager Pro - Server.msi"
