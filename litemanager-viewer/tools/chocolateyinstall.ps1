74505bb94ec074d3d87705be0155c1eff9dda182
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-01-26 12:55:33 +0300

    Update chocolateyinstall.ps1

--- litemanager-viewer/tools/chocolateyinstall.ps1
+++ litemanager-viewer/tools/chocolateyinstall.ps1
@@ -1,2 +1,2 @@
 ﻿Install-ChocolateyZipPackage "$env:chocolateyPackageName" 'http://litemanager.com/soft/litemanager_4.8.zip' "$env:TMP" -Checksum '5e27c47fac38ef31467776b3872a2033c599d3cbd91cfbd5109e84509f0e14b2' -ChecksumType 'sha256'
-Install-ChocolateyPackage "$env:chocolateyPackageName" 'msi' '/qn /norestart' "$env:TMP\LiteManager Pro - Viewer.msi" -Checksum 'bb6e36dd6ee043fb2e42b3bde6e54185e0c36bb6cc7e13ec142acf67dcf980b4' -ChecksumType 'sha256'
+Install-ChocolateyInstallPackage "$env:chocolateyPackageName" 'msi' '/qn /norestart' "$env:TMP\LiteManager Pro - Viewer.msi"
