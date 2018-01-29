95508554582e1abeeb1b71e3b2a5f70e2d6d2e50
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-01-30 00:04:07 +0400

    [ci push] Init 2.5.3499

--- advanced-ip-scanner/tools/chocolateyinstall.ps1
+++ advanced-ip-scanner/tools/chocolateyinstall.ps1
@@ -1,1 +1,9 @@
-﻿Install-ChocolateyPackage "$env:chocolateyPackageName" 'exe' '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' 'https://www.advanced-ip-scanner.com/download/ipscan25.exe' -Checksum '14e67c39fc99c315480f3e916c17fde6df5952bde763ef0c9f44cdc0bdabfec3' -ChecksumType 'sha256'
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'exe'
+  SilentArgs             = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  url                    = 'https://www.advanced-ip-scanner.com/download/Advanced_IP_Scanner_2.5.3499.exe'
+  checksum               = '73a638af070b53749e9a81c95efc78449c2ac12f186c0240c14b398666f091dd'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyPackage @packageArgs
