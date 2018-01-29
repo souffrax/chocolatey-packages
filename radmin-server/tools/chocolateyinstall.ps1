ac5955e73c5075d276e3212140bd60a72e20dc98
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-01-29 23:47:41 +0400

    [ci push] Init 3.5.2.1

--- radmin-server/tools/chocolateyinstall.ps1
+++ radmin-server/tools/chocolateyinstall.ps1
@@ -1,1 +1,9 @@
-﻿Install-ChocolateyPackage "$env:chocolateyPackageName" 'msi' '/qn /norestart' 'https://www.radmin.ru/download/Radmin_Server_3.5.1_RU.msi' -Checksum '5eb5bc1a2c779602a3606a91245a52201047ae3c5e61efd38df16e40850c1d15' -ChecksumType 'sha256'
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'msi'
+  SilentArgs             = '/qn /norestart'
+  url                    = 'https://www.radmin.com/download/Radmin_Server_3.5.2.1_EN.msi'
+  checksum               = '85844ae7394f2cf907b6378b415e77f7e29069c7e791598cf0985adf4f53320e'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyPackage @packageArgs
