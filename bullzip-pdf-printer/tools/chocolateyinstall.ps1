22448b7723be5b51e83ffb23be71dbb35d0eb5ea
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-01-29 12:35:15 +0300

    Init 11.4.0.2674

---
+++ bullzip-pdf-printer/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'exe'
+  SilentArgs             = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  url                    = 'http://cdn.bullzip.com/download/pdf_free/Setup_BullzipPDFPrinter_11_4_0_2674_FREE.exe'
+  checksum               = 'a1b970e2173b79b9543ffe7ec33ac3931117f71cb14125ffe56df9e1a32d1b79'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyPackage @packageArgs
