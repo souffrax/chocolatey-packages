011fb53d693de3b76c2a767dde0d2fb5fb2dbc2f
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-02-05 13:30:16 +0300

    Init 1.0.8

---
+++ kingston-ssd-toolbox/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'exe'
+  SilentArgs             = '-i console'
+  url                    = 'https://media.kingston.com/support/downloads/KingstonSSDToolbox.exe'
+  checksum               = 'e8549412b862be8711447cc6449459d17fb512a9c258d72c17d6188488a3648a'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyPackage @packageArgs
