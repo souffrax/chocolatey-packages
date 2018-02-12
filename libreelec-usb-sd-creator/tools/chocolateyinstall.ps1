3ccf9b5ca5fd8cc6aa0d3247695fe0eb76da6579
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-02-12 23:07:58 +0400

    Init 1.3

---
+++ libreelec-usb-sd-creator/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileFullPath           = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\LibreELEC.USB-SD.Creator.exe"
+  url                    = 'http://releases.libreelec.tv/LibreELEC.USB-SD.Creator.Win32.exe'
+  checksum               = 'c4b75de0612f38d0305fd68f70c95d8863b2d0837517390deee3ec7e95f8600e'
+  checksumType           = 'sha256'
+}
+Get-ChocolateyWebFile @packageArgs
--- mailru-agent/tools/chocolateyinstall.ps1
+++
@@ -1,2 +0,0 @@
-﻿#Server geneate installer executable on-the-fly, so no checksums
-Install-ChocolateyPackage "$env:chocolateyPackageName" 'exe' '' 'https://rfr.agent.mail.ru/magent.exe'
