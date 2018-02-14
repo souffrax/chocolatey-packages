28a504d4a6e275f22499a725eca511851848e0ba
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-02-14 17:52:30 +0300

    [ci push] Init 1.1

---
+++ rtadmin/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileFullPath           = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\rtAdmin.exe"
+  url                    = 'https://download.rutoken.ru/Rutoken/Utilites/rtadmin/1.1/windows/x86/rtAdmin.exe'
+  checksum               = '0b82d0352a216303b31213634ef80c042f8825fcffbc5d1fd45bcae85f76051d'
+  checksumType           = 'sha256'
+}
+Get-ChocolateyWebFile @packageArgs
