b2d8c3a2091d09656be554a558cd52ce33dc1c2b
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-02-14 17:52:30 +0300

    [ci push] Init 1.4.0.42

---
+++ rutoken-web-tool/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileFullPath           = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\rtwUtil.exe"
+  url                    = 'https://download.rutoken.ru/Rutoken_Web/utilities/1.4.0.42/Windows/rtwUtil.exe'
+  checksum               = 'cdfb56bc92012cb6a99e76018b0edcb037327626c8c8f982d48de7228cee05c5'
+  checksumType           = 'sha256'
+}
+Get-ChocolateyWebFile @packageArgs
