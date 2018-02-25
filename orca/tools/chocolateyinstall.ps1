51cb3f7ed6829371286eb3cb7b9dc13606b9b030
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-02-25 21:59:03 +0400

    [ci push] Init 3.1.3790.0000

--- orca/tools/chocolateyinstall.ps1
+++ orca/tools/chocolateyinstall.ps1
@@ -1,12 +1,25 @@
-﻿$packageArgs = @{
+﻿mkdir -f "$env:TMP\$Env:ChocolateyPackageName\$Env:ChocolateyPackageVersion"
+
+echo y > "$env:TMP\$Env:ChocolateyPackageName\$Env:ChocolateyPackageVersion\PSDK-FULL.14.cab"
+echo y > "$env:TMP\$Env:ChocolateyPackageName\$Env:ChocolateyPackageVersion\PSDK-FULL.16.cab"
+
+$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
-  url                    = 'https://download.microsoft.com/download/platformsdk/sdk/update/win98mexp/en-us/3718.1/msisdk-common.3.0.cab'
-  checksum               = 'b0493289274d931bc194d9d2716f9526890be822e4f107448cd3c59402a70a0f'
+  url                    = 'https://download.microsoft.com/download/5/b/9/5b9fb8b2-9238-491c-878a-0ae7d24591bd/PSDK-FULL.15.cab'
+  checksum               = 'c8aab17ec81078ffe169c8839262e83387aaa1d9317d4fabdd45dee4299067b7'
   checksumType           = 'sha256'
   UnzipLocation          = "$env:TMP"
-  SpecificFolder         = 'Orca_Msi.FD66E721_5AA0_41BC_AA26_1EC8F7FA1175'
+  SpecificFolder         = 'Setup\PSDK-SDK_MSI_BIN-common.0.cab'
 }
 Install-ChocolateyZipPackage @packageArgs
+
+$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileFullPath           = "$env:TMP\Setup\PSDK-SDK_MSI_BIN-common.0.cab"
+  Destination            = "$env:TMP"
+  SpecificFolder         = "Orca_Msi.FD66E721_5AA0_41BC_AA26_1EC8F7FA1175"
+}
+Get-ChocolateyUnzip @packageArgs
 
 $packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
