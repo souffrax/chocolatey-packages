cd0ec5d85b7143f72d3d94353cb4e2f6e8166e72
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-02-25 22:34:10 +0400

    [ci push] Init 3.0.3790.0000

--- orca/tools/chocolateyinstall.ps1
+++ orca/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
-  url                    = 'https://download.microsoft.com/download/platformsdk/sdk/update/win98mexp/en-us/3790.0/msisdk-common.3.0.cab'
-  checksum               = '6374c551192004abdb7960024309059e12a2bb83a2261165c50c75cb83648bf3'
+  url                    = 'https://download.microsoft.com/download/platformsdk/sdk/update/win98mexp/en-us/2600.2180/PSDK-SDK_MSI_BIN-common.0.cab'
+  checksum               = '43c5d39548ad24bc2d45ca2e4b6f47c0394388ccee71af7ed7c8a2dd71b48459'
   checksumType           = 'sha256'
   UnzipLocation          = "$env:TMP"
-  SpecificFolder         = 'Orca_Msi.FD66E721_5AA0_41BC_AA26_1EC8F7FA1175'
+  SpecificFolder         = 'Orca_Msi.A56930A2_F5D6_4E2A_BF20_10057BD915EB'
 }
 Install-ChocolateyZipPackage @packageArgs
 
@@ -12,6 +12,6 @@ $packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
   FileType               = 'msi'
   SilentArgs             = '/qn /norestart'
-  File                   = "$env:TMP\Orca_Msi.FD66E721_5AA0_41BC_AA26_1EC8F7FA1175"
+  File                   = "$env:TMP\Orca_Msi.A56930A2_F5D6_4E2A_BF20_10057BD915EB"
 }
 Install-ChocolateyInstallPackage @packageArgs
