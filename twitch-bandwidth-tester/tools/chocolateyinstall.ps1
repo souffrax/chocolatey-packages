2f83dd809f46c09a02d87ec6485a244f61755426
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-03-03 12:24:59 +0400

    [ci push] Init 1.40

--- twitch-bandwidth-tester/tools/chocolateyinstall.ps1
+++ twitch-bandwidth-tester/tools/chocolateyinstall.ps1
@@ -1,1 +1,8 @@
-﻿Install-ChocolateyZipPackage "$env:chocolateyPackageName" 'https://r-1.ch/TwitchTest.zip' "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)" -Checksum '2d105b4cb20f6acc3cf3bf5134dc24eddeb9f99e114ec1c8bfe714182b56e435' -ChecksumType 'sha256'
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  url                    = 'https://r1ch.net/assets/twitchtest/twitchtest-1.4.zip'
+  UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+  checksum               = '593e81b4d11446a6237c9346ef747bfb233a7bbb04948968f0d56ae22a9fae66'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyZipPackage @packageArgs
