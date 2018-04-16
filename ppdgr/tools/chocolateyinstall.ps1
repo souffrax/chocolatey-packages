c456880a79540da4e3bc1096a6d6308cd63d96c2
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-04-16 12:41:57 +0300

    [ci push] Init 1.4.9

--- ppdgr/tools/chocolateyinstall.ps1
+++ ppdgr/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
   packageName            = "$env:chocolateyPackageName"
   FileType               = 'exe'
   SilentArgs             = '/s /sp"/qn /norestart"'
-  url                    = 'https://www.gnivc.ru/html/gnivcsoft/ppdgr/1.4.8/Setup_PPDGR_full.exe'
-  checksum               = 'df6e6f4080544b0be47d8a85426fbf6ec31de64d8de96a47c0276d6edeaf507e'
+  url                    = 'https://www.gnivc.ru/html/gnivcsoft/ppdgr/1.4.9/Setup_PPDGR_full.exe'
+  checksum               = '3ee30b3ed946b9bdc2e18835a2cd8878a6ce824190d965d11129d72a96f2da2e'
   checksumType           = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
