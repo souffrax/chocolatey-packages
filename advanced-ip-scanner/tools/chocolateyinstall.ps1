02f6a010c8b805034ce1ee3dd3f8e4fb106bb26b
Elpatii <4057711+Elpatii@users.noreply.github.com>
2018-05-16 15:00:22 +0200

    Update for the latest version 2.5.3581
    Updated the link and the checksum

--- advanced-ip-scanner/tools/chocolateyinstall.ps1
+++ advanced-ip-scanner/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
   packageName            = "$env:chocolateyPackageName"
   FileType               = 'exe'
   SilentArgs             = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-  url                    = 'https://www.advanced-ip-scanner.com/download/Advanced_IP_Scanner_2.5.3499.exe'
-  checksum               = '73a638af070b53749e9a81c95efc78449c2ac12f186c0240c14b398666f091dd'
+  url                    = 'https://www.advanced-ip-scanner.com/download/Advanced_IP_Scanner_2.5.3581.exe'
+  checksum               = '49AFD1429544C5F722C188C299287014E4D24AD8C5F32EB0AA9D8B8C32FC03BE'
   checksumType           = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
