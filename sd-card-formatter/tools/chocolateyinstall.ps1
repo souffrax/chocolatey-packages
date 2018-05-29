c0351b97ad68949e4f7963f0f5e00651891c5737
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-05-29 15:24:17 +0300

    [ci push] Init 5.0.0.0

---
+++ sd-card-formatter/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'exe'
+  SilentArgs             = '/s /v"/qn /norestart"'
+  url                    = 'https://www.sdcard.org/downloads/formatter_4/eula_windows/SD_CardFormatter0500SetupEN.exe'
+  checksum               = '26d2494281ef4bded1c50a64ad2aa9fa5edff38d9a0e708f70fba094f6ba901e'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyPackage @packageArgs
