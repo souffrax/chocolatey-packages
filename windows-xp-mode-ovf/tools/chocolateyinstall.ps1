1ad4466985c84b20ac726d017e108c126f668a58
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-06-01 00:50:15 +0300

    Upd

--- windows-xp-mode-ovf/tools/chocolateyinstall.ps1
+++ windows-xp-mode-ovf/tools/chocolateyinstall.ps1
@@ -10,14 +10,14 @@ Install-ChocolateyZipPackage @packageArgs
 
 $packageArgs = @{
   FileFullPath           = "$env:TMP\sources\xpm"
-  Destination            = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+  Destination            = "$env:TMP"
   SpecificFolder         = 'VirtualXPVHD'
 }
 Get-ChocolateyUnzip @packageArgs
 
-mv "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\VirtualXPVHD" "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\VirtualXPVHD.vhd"
+mv "$env:TMP\VirtualXPVHD" "$env:TMP\VirtualXPVHD.vhd"
 
 VBoxManage createvm --name xpm --register --ostype WindowsXP --uuid b6447b49-fe2f-4c2f-90ec-a9eb23233f94
 VBoxManage storagectl b6447b49-fe2f-4c2f-90ec-a9eb23233f94 --name IDE --add ide
-VBoxManage storageattach b6447b49-fe2f-4c2f-90ec-a9eb23233f94 --storagectl IDE --medium "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\VirtualXPVHD.vhd" --port 0 --device 0 --type hdd
+VBoxManage storageattach b6447b49-fe2f-4c2f-90ec-a9eb23233f94 --storagectl IDE --medium "$env:TMP\VirtualXPVHD.vhd" --port 0 --device 0 --type hdd
 VBoxManage export b6447b49-fe2f-4c2f-90ec-a9eb23233f94 --output "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\xpm.ovf"
