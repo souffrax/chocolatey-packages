caf9b137f3ec145b5e568fce314b9bd2825aa0e7
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-06-01 17:29:10 +0300

    Upd

---
+++ windows-xp-mode-packer/tools/chocolateyinstall.ps1
@@ -0,0 +1,130 @@
+﻿function New-IsoFile  
+{  
+  <#  
+   .Synopsis  
+    Creates a new .iso file  
+   .Description  
+    The New-IsoFile cmdlet creates a new .iso file containing content from chosen folders  
+   .Example  
+    New-IsoFile "c:\tools","c:Downloads\utils"  
+    This command creates a .iso file in $env:temp folder (default location) that contains c:\tools and c:\downloads\utils folders. The folders themselves are included at the root of the .iso image.  
+   .Example 
+    New-IsoFile -FromClipboard -Verbose 
+    Before running this command, select and copy (Ctrl-C) files/folders in Explorer first.  
+   .Example  
+    dir c:\WinPE | New-IsoFile -Path c:\temp\WinPE.iso -BootFile "${env:ProgramFiles(x86)}\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\amd64\Oscdimg\efisys.bin" -Media DVDPLUSR -Title "WinPE" 
+    This command creates a bootable .iso file containing the content from c:\WinPE folder, but the folder itself isn't included. Boot file etfsboot.com can be found in Windows ADK. Refer to IMAPI_MEDIA_PHYSICAL_TYPE enumeration for possible media types: http://msdn.microsoft.com/en-us/library/windows/desktop/aa366217(v=vs.85).aspx  
+   .Notes 
+    NAME:  New-IsoFile  
+    AUTHOR: Chris Wu 
+    LASTEDIT: 03/23/2016 14:46:50  
+ #>  
+  
+  [CmdletBinding(DefaultParameterSetName='Source')]Param( 
+    [parameter(Position=1,Mandatory=$true,ValueFromPipeline=$true, ParameterSetName='Source')]$Source,  
+    [parameter(Position=2)][string]$Path = "$env:temp\$((Get-Date).ToString('yyyyMMdd-HHmmss.ffff')).iso",  
+    [ValidateScript({Test-Path -LiteralPath $_ -PathType Leaf})][string]$BootFile = $null, 
+    [ValidateSet('CDR','CDRW','DVDRAM','DVDPLUSR','DVDPLUSRW','DVDPLUSR_DUALLAYER','DVDDASHR','DVDDASHRW','DVDDASHR_DUALLAYER','DISK','DVDPLUSRW_DUALLAYER','BDR','BDRE')][string] $Media = 'DVDPLUSRW_DUALLAYER', 
+    [string]$Title = (Get-Date).ToString("yyyyMMdd-HHmmss.ffff"),  
+    [switch]$Force, 
+    [parameter(ParameterSetName='Clipboard')][switch]$FromClipboard 
+  ) 
+ 
+  Begin {  
+    ($cp = new-object System.CodeDom.Compiler.CompilerParameters).CompilerOptions = '/unsafe' 
+    if (!('ISOFile' -as [type])) {  
+      Add-Type -CompilerParameters $cp -TypeDefinition @' 
+public class ISOFile  
+{ 
+  public unsafe static void Create(string Path, object Stream, int BlockSize, int TotalBlocks)  
+  {  
+    int bytes = 0;  
+    byte[] buf = new byte[BlockSize];  
+    var ptr = (System.IntPtr)(&bytes);  
+    var o = System.IO.File.OpenWrite(Path);  
+    var i = Stream as System.Runtime.InteropServices.ComTypes.IStream;  
+  
+    if (o != null) { 
+      while (TotalBlocks-- > 0) {  
+        i.Read(buf, BlockSize, ptr); o.Write(buf, 0, bytes);  
+      }  
+      o.Flush(); o.Close();  
+    } 
+  } 
+}  
+'@  
+    } 
+  
+    if ($BootFile) { 
+      if('BDR','BDRE' -contains $Media) { Write-Warning "Bootable image doesn't seem to work with media type $Media" } 
+      ($Stream = New-Object -ComObject ADODB.Stream -Property @{Type=1}).Open()  # adFileTypeBinary 
+      $Stream.LoadFromFile((Get-Item -LiteralPath $BootFile).Fullname) 
+      ($Boot = New-Object -ComObject IMAPI2FS.BootOptions).AssignBootImage($Stream) 
+    } 
+ 
+    $MediaType = @('UNKNOWN','CDROM','CDR','CDRW','DVDROM','DVDRAM','DVDPLUSR','DVDPLUSRW','DVDPLUSR_DUALLAYER','DVDDASHR','DVDDASHRW','DVDDASHR_DUALLAYER','DISK','DVDPLUSRW_DUALLAYER','HDDVDROM','HDDVDR','HDDVDRAM','BDROM','BDR','BDRE') 
+ 
+    Write-Verbose -Message "Selected media type is $Media with value $($MediaType.IndexOf($Media))" 
+    ($Image = New-Object -com IMAPI2FS.MsftFileSystemImage -Property @{VolumeName=$Title}).ChooseImageDefaultsForMediaType($MediaType.IndexOf($Media)) 
+  
+    if (!($Target = New-Item -Path $Path -ItemType File -Force:$Force -ErrorAction SilentlyContinue)) { Write-Error -Message "Cannot create file $Path. Use -Force parameter to overwrite if the target file already exists."; break } 
+  }  
+ 
+  Process { 
+    if($FromClipboard) { 
+      if($PSVersionTable.PSVersion.Major -lt 5) { Write-Error -Message 'The -FromClipboard parameter is only supported on PowerShell v5 or higher'; break } 
+      $Source = Get-Clipboard -Format FileDropList 
+    } 
+ 
+    foreach($item in $Source) { 
+      if($item -isnot [System.IO.FileInfo] -and $item -isnot [System.IO.DirectoryInfo]) { 
+        $item = Get-Item -LiteralPath $item 
+      } 
+ 
+      if($item) { 
+        Write-Verbose -Message "Adding item to the target image: $($item.FullName)" 
+        try { $Image.Root.AddTree($item.FullName, $true) } catch { Write-Error -Message ($_.Exception.Message.Trim() + ' Try a different media type.') } 
+      } 
+    } 
+  } 
+ 
+  End {  
+    if ($Boot) { $Image.BootImageOptions=$Boot }  
+    $Result = $Image.CreateResultImage()  
+    [ISOFile]::Create($Target.FullName,$Result.ImageStream,$Result.BlockSize,$Result.TotalBlocks) 
+    Write-Verbose -Message "Target image ($($Target.FullName)) has been created" 
+    $Target 
+  } 
+}
+
+$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  url                    = 'https://download.microsoft.com/download/7/2/C/72C7BAB7-2F32-4530-878A-292C20E1845A/WindowsXPMode_en-us.exe'
+  UnzipLocation          = "$env:TMP"
+  checksum               = '0388d943f037c832fc205f514f1c1c1f076963b1a9249af05b3bd001f09e713f'
+  checksumType           = 'sha256'
+  SpecificFolder         = 'sources\xpm'
+}
+Install-ChocolateyZipPackage @packageArgs
+
+$packageArgs = @{
+  FileFullPath           = "$env:TMP\sources\xpm"
+  Destination            = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+  SpecificFolder         = 'VirtualXPVHD'
+}
+Get-ChocolateyUnzip @packageArgs
+
+$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  url                    = 'https://download.microsoft.com/download/E/C/E/ECE99583-2003-455D-B681-68DB610B44A4/WindowsXP-KB968930-x86-ENG.exe'
+  FileFullPath           = "$env:TMP\WindowsXP-KB968930-x86-ENG.exe"
+  checksum               = '0ef2a9b4f500b66f418660e54e18f5f525ed8d0a4d7c50ce01c5d1d39767c00c'
+  checksumType           = 'sha256'
+}
+Get-ChocolateyWebFile @packageArgs
+
+ls "$env:TMP\WindowsXP-KB968930-x86-ENG.exe" | New-IsoFile -Path "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\WindowsXP-KB968930-x86-ENG.iso"
+
+cd "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+
+packer build xpm.json
