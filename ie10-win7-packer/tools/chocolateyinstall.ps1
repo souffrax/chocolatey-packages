355680de3281449d5850d2c64a9d0d68c1d133f2
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-06-04 12:23:59 +0300

    Init ie10-win7-packer

---
+++ ie10-win7-packer/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  url                    = 'https://az792536.vo.msecnd.net/vms/VMBuild_20150916/VirtualBox/IE10/IE10.Win7.VirtualBox.zip'
+  UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+  checksum               = 'ff8b51114d13c31742f39fc49a20b035bd5e25e9a35fd29cf41b73aa8fadbd3b'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyZipPackage @packageArgs
