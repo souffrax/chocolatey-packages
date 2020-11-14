305539a1fe325af00c56e957e6abe50cdd3f75e6
souffrax <florent76150@hotmail.fr>
2020-11-14 17:44:47 +0100

    Update chocolateyinstall.ps1
    Package broken.

--- realtek-card-reader-driver/tools/chocolateyinstall.ps1
+++ realtek-card-reader-driver/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
-  url                    = 'http://12244.wpc.azureedge.net/8012244/drivers/rtdrivers/pc/crc/0007-RtsXStor_10.0.370.188.zip'
+  url                    = 'https://realtekcdn.akamaized.net/rtdrivers/pc/crc/0007-RtsXStor_10.0.370.188.zip?__token__=exp=1605372115~acl=/rtdrivers/pc/crc/0007-RtsXStor_10.0.370.188.zip'
   checksum               = '5e054924cadd182c8feb28000688dff3db72885d8e42281971d0930d497d607a'
   checksumType           = 'sha256'
   UnzipLocation          = "$env:TMP"
